054274766b6e9761fa0c93f457165e0151982fa5
Platon <xnpltn@gmail.com>
2024-11-15 00:08:39 +0200

    Modifying Parameters

--- src-tauri/src/types/mod.rs
+++ src-tauri/src/types/mod.rs
@@ -1,29 +1,36 @@
 use serde::{self, Deserialize, Serialize};
-use sqlx::SqlitePool;
+use sqlx::{prelude::FromRow, SqlitePool};
 use std::sync::Arc;
 
 pub struct State {
     pub pool: Arc<SqlitePool>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, FromRow, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct RelativeIndividual {
     pub id: u16,
-    pub name: String,
-    pub age: Option<u8>,
+    pub first_name: String,
+    pub middle_name: Option<String>,
+    pub last_name: String,
+    pub birthday: Option<String>,
     pub sameness: Option<f32>,
+    #[sqlx(skip)]
     pub mother: Option<String>,
+    #[sqlx(skip)]
     pub father: Option<String>,
     pub phone: Option<String>,
     pub email: Option<String>,
+    pub sex: String,
     pub pinned: bool,
     pub lost_reason: Option<String>,
-    pub created_at: Option<String>,
-    pub updated_at: Option<String>,
+    pub swarthy: Option<u8>,
+    pub hotness: Option<u8>,
+    pub crazy: Option<u8>,
+    pub employable: Option<u8>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, FromRow, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FemaleIndividual {
     pub id: u16,
@@ -42,7 +49,7 @@ pub struct FemaleIndividual {
     pub updated_at: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, FromRow, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct EmployeeIndividual {
     pub id: u16,
@@ -59,7 +66,7 @@ pub struct EmployeeIndividual {
     pub updated_at: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, FromRow, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateRelativeParams {
     pub first_name: String,
@@ -80,14 +87,14 @@ pub struct CreateRelativeParams {
     pub employable: Option<u8>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FemaleRelatives {
-    names: Vec<String>,
+    pub names: Vec<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct MalesRelatives {
-    names: Vec<String>,
+    pub names: Vec<String>,
 }
